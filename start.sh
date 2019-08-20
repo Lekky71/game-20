a579b20bd4af2c4b97ed56a91d59e5c940ecdb25
Oluwaleke Fakorede <hashilekky@gmail.com>
2019-08-20 17:24:10 +0100

    edited start script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cd client
-npm install
-# Build the Vue project
-npm run build
+#cd client
+#npm install
+## Build the Vue project
+#npm run build
 cd ../api
 # start the API server
 npm install
